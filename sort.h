2ea87eb81a8e3585fe7dac0569c70c4a449fb714
SpacePistache <lukeholley@hotmail.com>
2024-12-13 16:07:20 +0100

    insertion fixed sort.h bad order

--- sort.h
+++ sort.h
@@ -3,6 +3,21 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+
 /**
  * print_array - Prints an array of integers
  *
@@ -45,19 +60,6 @@ list = list->next;
 printf("\n");
 }
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
-} listint_t;
 
 void insertion_sort_list(listint_t **list);
 
