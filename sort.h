de165af3e662827cd3ff4c1e6bf39947997a521e
SpacePistache <lukeholley@hotmail.com>
2024-12-13 18:21:44 +0100

    fixed missing prototype

--- sort.h
+++ sort.h
@@ -21,4 +21,8 @@ void print_array(const int *array, size_t size);
 
 void print_list(const listint_t *list);
 
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+
+void insertion_sort_list(listint_t **list);
+
 #endif
